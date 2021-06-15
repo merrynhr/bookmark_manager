fa0a02fb0da67ebaec8a251dbe8974e7b138e9aa
Camilla <camilla.lima000@gmail.com>
2021-06-15 12:00:40 +0100

    Interacting with Postgres from Ruby

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,7 +3,7 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
    it 'returns all bookmarks' do
-    bookmarks = Bookmarks.all 
+    bookmarks = Bookmark.all 
 
     expect(bookmarks).to include("http://www.makersacademy.com")
     expect(bookmarks).to include("http://www.destroyallsoftware.com")
