1f7c0bd2e7bcc7eb6835da70de89c2fd373e9b2a
Merryn <merrynhr@gmail.com>
2021-06-16 12:36:08 +0100

    still working on step 9

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,6 +3,15 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
    it 'returns all bookmarks' do
+
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+
     bookmarks = Bookmark.all 
 
     expect(bookmarks).to include("http://www.makersacademy.com")
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,5 +1,18 @@
+feature 'Viewing bookmarks' do 
+  feature 'visiting the homepage' do 
+    
+
 feature "Viewing the bookmarks" do
   scenario 'The user can see the bookmarks' do
+    
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+
     visit('/bookmarks') #the page visited
 
 
